d807cfcb1576b22a8a2940e14b537e12ad2402fa
kawsantos25 <kawane.santos20@escola.pr.gov.br>
2025-10-03 11:48:21 -0300

    .

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
+const avanca = document.querySelectorA11('.btn-proximo');
+
+avanca.forEach(button => {
+    button.addEventlistener('click', function(){
+       const atual = documnt.queryselector('.ativo');
+       const proximoPasso = 'passo-' + this.getAttribute('data-proximo');
+
+       atual.classList.remove('ativo');
+       document.getElementById(proximopasso).classList.add('ativo'):
+    })
+})
+  
 
